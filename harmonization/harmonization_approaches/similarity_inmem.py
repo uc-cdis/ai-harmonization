07176de67a59159d3bf21d615e94befbec72e140
avantol <avantol@uchicago.edu>
2025-08-21 13:37:39 -0500

    feat(data-curation): initial notebook for tookit, cleanup existing harmonization approaches

--- harmonization/harmonization_approaches/similarity_inmem.py
+++ harmonization/harmonization_approaches/similarity_inmem.py
@@ -1,23 +1,13 @@
-import ast
-import json
 import logging
-import os
-import shutil
 
 from langchain_huggingface import HuggingFaceEmbeddings
 
 from harmonization.cde import (
-    EXAMPLE_AI_MODEL_OUTPUT_JSON,
     TEMP_DIR,
     add_documents_to_vectorstore,
-    get_cdes_as_langchain_documents,
-    get_data_model_as_langchain_documents,
     get_gen3_data_model_as_langchain_documents,
-    get_gen3_data_model_as_node_prop_descriptions,
-    get_interactive_table_for_suggestions,
     get_langchain_vectorstore_and_persistent_client,
     get_similar_documents,
-    get_similar_documents_as_string,
 )
 from harmonization.harmonization_approaches.base import (
     HarmonizationApproach,
@@ -26,13 +16,19 @@ from harmonization.harmonization_approaches.base import (
 )
 
 
+class ExistingVectorstoreException(BaseException):
+    pass
+
+
 class SimilaritySearchInMemoryVectorDb(HarmonizationApproach):
 
     def __init__(
         self,
         vectordb_persist_directory_name: str,
+        input_target_model: dict,
         input_target_model_type: str = "gen3",
-        embedding_function: HuggingFaceEmbeddings = None,
+        embedding_function: HuggingFaceEmbeddings | None = None,
+        force_vectorstore_recreation: bool = False,
     ):
         super().__init__()
 
@@ -56,16 +52,26 @@ class SimilaritySearchInMemoryVectorDb(HarmonizationApproach):
         self.vectorstore = vectorstore
         self.persistent_client = persistent_client
 
-    def add_target_to_vector_database(self, input_target_model, force_recreation=False):
+        try:
+            self._add_target_to_vector_database(
+                input_target_model, force_recreation=force_vectorstore_recreation
+            )
+        except ExistingVectorstoreException:
+            logging.info("vectorstore already exists, NOT recreating...")
+            pass
+
+    def _add_target_to_vector_database(
+        self, input_target_model, force_recreation=False
+    ):
         if force_recreation:
             all_ids = self.vectorstore.get()["ids"]
             if all_ids:
                 self.vectorstore.delete(ids=all_ids)
 
         if len(self.vectorstore.get()["ids"]) != 0:
-            raise Exception(
+            raise ExistingVectorstoreException(
                 f"Vectorstore in persist directory {self.vectordb_persist_directory_name} already has data here: {TEMP_DIR}, "
-                "aborting re-adding. Delete the persist directory or use a new one."
+                "aborting re-adding. Delete the persist directory, use a new one, or force recreation."
             )
 
         target_docs = get_gen3_data_model_as_langchain_documents(input_target_model)
@@ -75,10 +81,11 @@ class SimilaritySearchInMemoryVectorDb(HarmonizationApproach):
         )
 
     def get_harmonization_suggestions(
-        self, input_source_model, input_target_model, score_threshold=0.5, k=4, **kwargs
+        self, input_source_model, input_target_model, **kwargs
     ):
+        # note: k and score_threshold are in kwargs
         suggestions_for_output_model = self._get_suggestions_for_ai_model_output(
-            input_source_model, score_threshold=score_threshold, k=k, **kwargs
+            input_source_model, **kwargs
         )
         suggestions = []
         for node_property_desc, suggested_docs in suggestions_for_output_model.items():

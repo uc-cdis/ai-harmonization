fc78e49d0c445fa64af46752327b3f97ce6b512e
Victoria Zaksas <vzpgb@uchicago.edu>
2025-10-08 14:36:27 -0500

    feat(baai-bge): Add BGE embedding

--- harmonization/harmonization_approaches/embeddings.py
+++ harmonization/harmonization_approaches/embeddings.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModel
+
 
 class BaseEmbeddings:
     def __init__(self, model_name, device=None, trust_remote_code=False):
@@ -10,10 +12,12 @@ class BaseEmbeddings:
         self.device = torch.device(
             device if device else ("cuda" if torch.cuda.is_available() else "cpu")
         )
-        self.model = AutoModel.from_pretrained(
-            model_name, trust_remote_code=trust_remote_code
-        ).to(self.device).eval()
-    
+        self.model = (
+            AutoModel.from_pretrained(model_name, trust_remote_code=trust_remote_code)
+            .to(self.device)
+            .eval()
+        )
+
     def embed_query(self, text):
         return self.embed_documents([text])[0]
 
@@ -27,6 +31,7 @@ class BaseEmbeddings:
 
     def embed_documents(self, texts):
         raise NotImplementedError("Implement in subclass.")
+
 
 class MedGemmaEmbeddings(BaseEmbeddings):
     def __init__(self, model_name="google/medgemma-4b-it", device=None):
@@ -47,6 +52,7 @@ class MedGemmaEmbeddings(BaseEmbeddings):
             )
             pooled = self.mean_pool(last_hidden, mask)
             return pooled.cpu().numpy().tolist()
+
 
 class QwenEmbeddings(BaseEmbeddings):
     def __init__(self, model_name="Qwen/Qwen3-Embedding-0.6B", device=None):
@@ -72,3 +78,20 @@ class QwenEmbeddings(BaseEmbeddings):
             )
             pooled = self.mean_pool(last_hidden, mask)
             return pooled.cpu().numpy().tolist()
+
+
+class BGEEmbeddings(BaseEmbeddings):
+    def __init__(self, model_name="BAAI/bge-large-en-v1.5", device=None):
+        super().__init__(model_name, device=device)
+
+    def embed_documents(self, texts):
+        inputs = self.tokenizer(
+            texts, padding=True, truncation=True, max_length=512, return_tensors="pt"
+        ).to(self.device)
+        with torch.no_grad():
+            model_output = self.model(**inputs)
+            mask = inputs["attention_mask"].unsqueeze(-1)
+            pooled = self.mean_pool(model_output.last_hidden_state, mask)
+        pooled = pooled.cpu().numpy()
+        pooled = pooled / np.linalg.norm(pooled, axis=1, keepdims=True)
+        return pooled.tolist()
